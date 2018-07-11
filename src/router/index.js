b8a0955ae7895d75d8503acc74dc926e58d53eee
zkassing <kassing@live.cn>
2018-07-11 16:07:55 +0800

    初始化项目，添加vuex

--- src/router/index.js
+++ src/router/index.js
@@ -1,33 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Panel from '@/components/Panel'
-import Sheet from '@/components/Sheet'
-import Detail from '@/components/Detail'
-import Play from '@/components/Play'
+
 Vue.use(Router)
 
 export default new Router({
-  routes: [
-    {
-      path: '/',
-      component: Panel,
-      redirect: '/sheet',
-      children: [
-        {
-          path: 'sheet',
-          component: Sheet
-        }
-      ]
-    },
-    {
-      path: '/sheet/datail/:id',
-      name: 'detail',
-      component: Detail,
-    },
-    {
-      path: '/music/play/:id',
-      name: 'play',
-      component: Play
-    }
-  ]
+  routes: []
 })
